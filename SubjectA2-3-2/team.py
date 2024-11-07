4ee3550ac2355f37511e069b428a7a2544049309
Seonyeol-Jeong <wjdtjsduf1@dgu.ac.kr>
2024-11-07 22:30:25 +0900

    feat: modify index.html, team.py for layout

--- SubjectA2-3-2/team.py
+++ SubjectA2-3-2/team.py
@@ -1,14 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # 세션 사용을 위해 필요
 
-# 최초 메인 페이지를 보여주는 루트 경로
 @app.route('/')
 def index():
-    return render_template('index.html')  # 위 HTML 코드를 form.html 파일로 저장해야 합니다
+    result = session.get('result', [])  # 세션에서 결과 데이터 가져오기
+    return render_template('index.html', result=result)
 
 # 학생 정보를 입력하는 경로
-
 @app.route('/input')
 def input():
    return render_template('input_a.html')
@@ -17,26 +17,18 @@ def input():
 @app.route('/result', methods=['POST'])
 def result():
     # 각 학생의 이름과 학번 데이터를 리스트로 받음
-   names = request.form.getlist('Name[]')
-   student_numbers = request.form.getlist('StudentNumber[]')
-   genders = [request.form.get(f'Gender[{i}]') for i in range(len(names))]
-   majors = request.form.getlist('Major[]')
-   languages = [', '.join(request.form.getlist(f'language[{i}][]')) for i in range(len(names))]
+    names = request.form.getlist('Name[]')
+    student_numbers = request.form.getlist('StudentNumber[]')
+    genders = [request.form.get(f'Gender[{i}]') for i in range(len(names))]
+    majors = request.form.getlist('Major[]')
+    languages = [', '.join(request.form.getlist(f'language[{i}][]')) for i in range(len(names))]
 
-
-   # zip 객체를 리스트로 변환하여 템플릿에 전달
-   zipped_result = list(zip(names, student_numbers, genders, majors, languages))
-   
-   #  # 디버깅용입니다 ㅎ
-   # print("Names:", names)
-   # print("Student Numbers:", student_numbers)
-   # print("Genders:", genders)
-   # print("Majors:", majors)
-   # print("Languages:", languages)
-   # print("Zipped Result:", zipped_result)
-   
-   # 데이터를 템플릿으로 전달하여 출력 페이지 생성
-   return render_template('result_contact.html', result=zipped_result)
+    # zip 객체를 리스트로 변환하여 템플릿에 전달
+    zipped_result = list(zip(names, student_numbers, genders, majors, languages))
+    session['result'] = zipped_result
+    
+    # index.html로 데이터를 전달하여 메인 페이지에서 결과 출력
+    return render_template('index.html', result=zipped_result)
 
 @app.route('/contact')
 def contact_info():
